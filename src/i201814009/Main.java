8475235cafc507625fbcae3bebdde512f50ad409
BansonJ <wjdtmdgus0313@gmail.com>
2022-10-24 15:51:55 +0900

    Hello.java 수정1

--- src/i201814009/Main.java
+++ src/i201814009/Main.java
@@ -1,5 +1,6 @@
+package i201814009;
 public class Main {
      public static void main(String[] args) {
-         System.out.println("hello world"); 
+         System.out.println("hello world");
     }
  }
